a0f53ffe3cfb1389b68bf43e7ec6b03b34f9f74c
Ghaidaa <38421568+GH8L@users.noreply.github.com>
2021-10-08 21:47:40 +0300

    Add files via upload

---
+++ Hello_World.py
@@ -0,0 +1,29 @@
+
+
+
+#1
+print("Hello World");
+
+#2
+Name = "Ghaida";
+print("Hello",Name);
+
+#3
+Name =88 ;
+print("Hello",Name);
+
+#4
+fave_food1 = "Italian food";
+fave_food2 = "Pasta" ;
+
+print("I Like to eat {} , Mostly {} ".format(fave_food1 , fave_food2) );
+
+
+#String method
+
+
+myFN= ("John", "Peter", "Vicky")
+
+x = "#".join(myFN)
+
+print(x)
